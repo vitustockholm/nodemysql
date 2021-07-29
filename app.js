552f4e5984349659255738feaf09630cfdce8182
Vitus <vitustockholm@gmail.com>
2021-07-29 10:34:26 +0300

    working

--- app.js
+++ app.js
@@ -1,54 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
-
-//--Connecting DB
+// Connecting DB
 let connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'my_first_db',
 });
-
-connection.connect(() => console.log('Connected to MYSQL'));
-
-//--Starting express app
+connection.connect(() => console.log('Connected MySQL...'));
+// Starting express app
 let app = express();
-
-//--Midllewares
-
+// -- middlewares
 app.use(cors());
 app.use(express.json());
-
-//Routes
-
-//GET
-
-//POST
+// Routes
+// GET
+// POST
+// -- create table
+app.post('/api/admin/createtable', (req, res) => {
+  const tableName = req.body.tableName;
+  connection.query(
+    `CREATE TABLE ${tableName}(ID int AUTO_INCREMENT, make VARCHAR(255), model VARCHAR(255), PRIMARY KEY (ID))`,
+    () => {
+      console.log(`${tableName} table created...`);
+    }
+  );
+});
+// -- add car to cars table
 app.post('/api/cars', (req, res) => {
-  //   console.log(req.body);
-
   let car = req.body;
-
-  let querry = connection.query('INSERT INTO cars SET ?', car, () => {
-    console.log('car added');
-    res.send('Car Added');
+  let query = connection.query('INSERT INTO cars SET ?', car, () => {
+    console.log('Car added');
+    res.send('Car added');
   });
 });
-
-
-//
-app.post('/api/posts', (req, res) => {
-    console.log(req.body);
-    let post = { title: `${req.body.title}`, body: `${req.body.body}` };
-    let sql = 'INSERT INTO posts SET ?';
-    let query = db.query(sql, post, (err, result) => {
-      if (err) throw err;
-      console.log(result);
-      res.send('Post from front-end added...');
-    });
-  });
-//
-
-
 app.listen(5000);
